f73208b00c800eb5e997433f9f3ebb8a40e98b2f
MrL013 <lev.rock@yandex.ru>
2023-10-25 00:10:32 +0300

    caesar and vigenre

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,35 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
 
+    k = shift % 26
+    Z = 90
+    z = 122
+    
+    for i in range(len(plaintext)):
+        n = ord(plaintext[i])
+        if plaintext[i].isalpha():
+            if plaintext[i].isupper():
+                if n + k <= Z:
+                    ciphertext += chr(n+k)
+                else:
+                    x = n + k - Z
+                    ciphertext += chr(64 + x)
+            else:
+                if n + k <= z:
+                    ciphertext += chr(n+k)
+                else:
+                    x = n + k - z
+                    ciphertext += chr(96 + x)
+
+        else:
+            ciphertext += plaintext[i]
+    return ciphertext
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +51,30 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    k = shift % 26
+    A = 65
+    a = 97
+
+    for i in range(len(ciphertext),):
+        n = ord(ciphertext[i])
+        if ciphertext[i].isalpha():
+            if ciphertext[i].isupper():
+                if n - k >= A:
+                    plaintext += chr(n-k)
+                else:
+                    x = A - (n-k)
+                    plaintext += chr(91 - x)
+            else:
+                if n - k >= a:
+                    plaintext += chr(n-k)
+                else:
+                    x = a - (n-k)
+                    plaintext += chr(123 - x)
+
+        else:
+            plaintext += ciphertext[i]
+    
     return plaintext
+
+
--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -8,14 +9,65 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    caps_lang = list('ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    lang = list('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz')
 
+    ciphertext = ""
+
+    for i in range(len(plaintext)):
+
+        if len(keyword) == len(plaintext):
+            if plaintext[i].isupper():
+                n_p = caps_lang.index(plaintext[i])
+                if keyword[i].isupper():
+                    n_k = caps_lang.index(keyword[i])
+                    ciphertext += caps_lang[n_k+n_p]
+                else:
+                    n_k = lang.index(keyword[i])
+                    ciphertext += lang[n_k+n_p]
+            else:
+                n_p = lang.index(plaintext[i])
+                if keyword[i].isupper():
+                    n_k = caps_lang.index(keyword[i])
+                    ciphertext += caps_lang[n_k+n_p]
+                else:
+                    n_k = lang.index(keyword[i])
+                    ciphertext += lang[n_k+n_p]
+
+        elif len(keyword) < len(plaintext):
+
+            keywords = list(keyword)
+            k = len(plaintext) - len(keyword)
+            K = k // len(keyword)
+            for _ in range(K):
+                keywords.extend(keywords)
+            n = k % len(keyword)
+            if n != 0:
+                keywords.extend(keywords[:n])
+
+            if plaintext[i].isupper():
+                n_p = caps_lang.index(plaintext[i])
+                if keywords[i % len(keywords)].isupper():
+                    n_k = caps_lang.index(keywords[i % len(keywords)])
+                    ciphertext += caps_lang[n_k + n_p]
+                else:
+                    n_k = lang.index(keywords[i % len(keywords)])
+                    ciphertext += lang[n_k + n_p]
+            else:
+                n_p = lang.index(plaintext[i])
+                if keywords[i % len(keywords)].isupper():
+                    n_k = caps_lang.index(keywords[i % len(keywords)])
+                    ciphertext += caps_lang[n_k + n_p]
+                else:
+                    n_k = lang.index(keywords[i % len(keywords)])
+                    ciphertext += lang[n_k + n_p]
+
+    return ciphertext
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -23,6 +75,58 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+
+    caps_lang = list('ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    lang = list('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz')
+    
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(ciphertext)):
+
+        if len(keyword) == len(ciphertext):
+            if ciphertext[i].isupper():
+                n_c = caps_lang.index(ciphertext[i])
+                if keyword[i].isupper():
+                    n_k = caps_lang.index(keyword[i])
+                    plaintext += caps_lang[n_c - n_k]
+                else:
+                    n_k = lang.index(keyword[i])
+                    plaintext += lang[n_c - n_k]
+            else:
+                n_c = lang.index(ciphertext[i])
+                if keyword[i].isupper():
+                    n_k = caps_lang.index(keyword[i])
+                    plaintext += caps_lang[n_c - n_k]
+                else:
+                    n_k = lang.index(keyword[i])
+                    plaintext += lang[n_c - n_k]
+
+        elif len(keyword) < len(ciphertext):
+            keywords = list(keyword)
+            k = len(ciphertext) - len(keyword)
+            K = k // len(keyword)
+            for _ in range(K):
+                keywords.extend(keywords)
+            n = k % len(keyword)
+            if n != 0:
+                keywords.extend(keywords[:n])
+
+            if ciphertext[i].isupper():
+                n_c = caps_lang.index(ciphertext[i])
+                if keywords[i % len(keywords)].isupper():
+                    n_k = caps_lang.index(keywords[i % len(keywords)])
+                    plaintext += caps_lang[n_c - n_k]
+                else:
+                    n_k = lang.index(keywords[i % len(keywords)])
+                    plaintext += lang[n_c - n_k]
+            else:
+                n_c = lang.index(ciphertext[i])
+                if keywords[i % len(keywords)].isupper():
+                    n_k = caps_lang.index(keywords[i % len(keywords)])
+                    plaintext += caps_lang[n - n_k]
+                else:
+                    n_k = lang.index(keywords[i % len(keywords)])
+                    plaintext += lang[n_c - n_k]
+
     return plaintext
+
