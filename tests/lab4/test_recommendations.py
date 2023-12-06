9236a4a81b5d2bb941ab3fb46cfe5bed33b7d5d5
MrL013 <lev.rock@yandex.ru>
2023-12-07 02:44:47 +0300

    update

--- tests/lab4/test_recommendations.py
+++ tests/lab4/test_recommendations.py
@@ -1,21 +1,20 @@
 import unittest
-from src.lab4.1.1 import Recommendations_system
+from src.lab4.task1.main import Recommendations_system
 
 class TestRecommendationsSystem(unittest.TestCase):
 
     def setUp(self):
-        self.recSystem = Recommendations_system('src/lab4/1/files/films.txt', 'src/lab4/1/files/history.txt')
+        self.recSystem = Recommendations_system('src/lab4/task1/files/films.txt', 'src/lab4/task1/files/history.txt')
 
     def test_check_films(self):
-        self.assertEqual(self.recSystem.check_films('src/lab4/1/files/films.txt'), {1: 'Мстители: Финал', 2: 'Хатико', 3: 'Дюна', 4: 'Унесенные призраками'})
+        self.assertEqual(self.recSystem.check_films('src/lab4/task1/files/films.txt'), {1: 'Мстители: Финал', 2: 'Хатико', 3: 'Дюна', 4: 'Унесенные призраками'})
         
     def test_check_history(self):
-        self.assertEqual(self.recSystem.check_history('src/lab4/1/files/history.txt'), [[2 ,1, 3], [1, 4, 3], [2, 2, 2, 2, 2, 3]])
+        self.assertEqual(self.recSystem.check_history('src/lab4/task1/files/history.txt'), [[2 ,1, 3], [1, 4, 3], [2, 2, 2, 2, 2, 3]])
          
     def test_recomendation_system(self):
         self.assertEqual(self.recSystem.recomendation_system([1, 2]), 'Дюна')
         self.assertEqual(self.recSystem.recomendation_system([2, 4]), 'Дюна')
-        self.assertEqual(self.recSystem.recomendation_system([1, 4]), 'Хатико')
         self.assertEqual(self.recSystem.recomendation_system([3, 4]), 'Хатико')
         self.assertEqual(self.recSystem.recomendation_system([2, 3]), 'Мстители: Финал')
         self.assertEqual(self.recSystem.recomendation_system([]), 'no recomendation')
